16cd03a39d50ff542663296556b297cb445a4715
mariiarubiin <134617905+mariiarubiin@users.noreply.github.com>
2023-05-28 15:46:39 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Maria Rubin
    # Additional Authors:
-   # <your GitHub username>
+   # <mariiarubiin>
 
    # Input:
    # p, principal amount
